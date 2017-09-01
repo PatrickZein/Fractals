272db54c02512eaa4267f53f91061825696d3278
PatrickZein <31516458+PatrickZein@users.noreply.github.com>
2017-09-01 07:13:50 +0200

    Detta är ett gammalt projekt som jag tycker om att experimentera med.

---
+++ Fraktaler/Function Mountain 2/Frak!.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+public class DrawPicture : Form
+{
+    int minpixWidth = 1;
+	int zoomFactor = 10;
+    int maxIter     = 1024;
+	int maxColour   = 1024;
+    int scrWidth    = 1920;
+    int scrHeight   = 1080;
+
+    double paramA = 0;
+    double paramB = 0;
+    
+    string functionType;
+    string graphName;
+
+	public int MaxColour { get => maxColour; set => maxColour = value; }
+
+	public DrawPicture()
+    {
+        // functionType = "Mandelbrot";
+         functionType = "z^2"; paramA = -0.8; paramB = 0.156;
+		// functionType = "z^3"; paramA = 0.4; paramB = 0;
+		// functionType = "z^4"; paramA = 0.484; paramB = 0;
+		// functionType = "z^5"; paramA = 0.544; paramB = 0;
+
+		// scrWidth = screen.Primaryscreen.Bounds.Width;
+		// scrHeight = screen.Primaryscreen.Bounds.Height;
+
+		// Initiera skärmen med att anropa grafikrutinen
+		if (functionType == "Mandelbrot") graphName = "Mandelbrot z'= z^2 + c";
+        else graphName = "Julia Set z' = " + functionType + " + " + Convert.ToString(paramA) + " + " + Convert.ToString(paramB) + "i";
+
+        this.Text = graphName;
+        this.Size = new Size(scrWidth, scrHeight);
+		//this.Opacity = .75;
+		//this.StartPosition = 0;
+		this.Paint += new PaintEventHandler(Paintgraph);
+    }
+
+     void Paintgraph(object sender, PaintEventArgs e)
+    {
+        // Rutans nedre vänstra hörn = (pixelX, pixelY)
+        for (int pixWidth = 8; pixWidth >= minpixWidth; pixWidth = pixWidth / 2)
+            for (int pixelX = 1; pixelX <= scrWidth; pixelX += pixWidth)
+                for (int pixelY = 1; pixelY <= scrHeight; pixelY += pixWidth)
+                {
+                    // Beräkna koordinater för en pixel
+                    double coorX = Convert.ToDouble(pixelX - scrWidth / 2)  / scrHeight * 2.8 / zoomFactor + 0.0000235;
+                    double coorY = Convert.ToDouble(pixelY - scrHeight / 2) / scrHeight * 2.8 / zoomFactor;
+                    
+                    // Beräkna iterationer för en pixel
+                    // double iterations = FrakIt(coorX - 1.9, coorY - 1.1, paramA, paramB)
+                    double iterations = FrakIt(coorX, coorY, paramA, paramB);
+
+                    // Välj färgnummer mellan 0 och 2048
+                    int landColor = Convert.ToInt16((iterations %  MaxColour) / MaxColour * 2048);
+                    if (landColor < 0) landColor = 0;
+
+                    // Greppa penseln
+                    Graphics g = e.Graphics;
+                    SolidBrush paintBrush = new SolidBrush(Color.White);
+
+                    // Ställ in rätt nyans på penseln
+                    int colorTrans = 0;
+                    int colorRed   = 0;
+                    int colorGreen = 0;
+                    int colorBlue  = 0;
+
+                    /* Black - Red */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = landColor; colorGreen = 0; colorBlue = 0; }
+                    else landColor = landColor - 256;
+                    /* Red - Yelow */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = 255; colorGreen = landColor; colorBlue = 0; }
+                    else landColor = landColor - 256;
+                    /* Yellow - Green */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = 255 - landColor; colorGreen = 255; colorBlue = 0; }
+                    else landColor = landColor - 256;
+                    /* Green - Teal */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = 0; colorGreen = 255; colorBlue = landColor; }
+                    else landColor = landColor - 256;
+                    /* Teal - Blue */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = 0; colorGreen = 255 - landColor; colorBlue = 255; }
+                    else landColor = landColor - 256;
+                    /* Blue - Purple */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = landColor; colorGreen = 0; colorBlue = 255; }
+                    else landColor = landColor - 256;
+                    /* Purple - White */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = 255; colorGreen = landColor; colorBlue = 255; }
+                    else landColor = landColor - 256;
+                    /* White - Black */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255; colorRed = 255 - landColor; colorGreen = 255 - landColor; colorBlue = 255 - landColor; }
+                    else landColor = landColor - 256;
+                    /* overflow --> Black */
+                    if (colorTrans == 0 && landColor >= 0 && landColor < 256) { colorTrans = 255 - landColor; colorRed = 0; colorGreen = 0; colorBlue = 0; }
+
+                    paintBrush.Color =
+                        Color.FromArgb(255, // Specifies the transparency of the color.
+                                       colorRed,   // Specifies the amount of red.
+                                       colorGreen, // specifies the amount of green.
+                                       colorBlue); // Specifies the amount of blue.
+
+                    e.Graphics. FillPolygon(paintBrush, new Point[]{
+                        new Point(pixelX, pixelY),
+                        new Point(pixelX + pixWidth, pixelY),
+                        new Point(pixelX + pixWidth, pixelY + pixWidth),
+                        new Point(pixelX, pixelY + pixWidth)});
+                 }
+    }
+
+    public double FrakIt(double x, double y, double constA, double constB)
+    {
+        int iteration = 0;
+        double a = x;
+        double b = y;
+        double aprim = 0;
+        double bprim = 0;
+        double modulus = 0;
+
+        if (functionType == "Mandelbrot")
+        {
+            // För Mandelbrot är alltid konstanterna = koordinaterna
+            constA = x;
+            constB = y;
+			aprim = a * a - b * b + constA;
+			bprim = 2 * a * b + constB;
+		}
+		do
+        {
+            iteration++;
+
+            if (functionType == "z^2" || functionType == "Mandelbrot")
+            { 
+                aprim = a * a - b * b + constA;
+                bprim = 2 * a * b + constB;
+            }
+            else if (functionType == "z^3")
+            {
+                aprim = a * a * a - 3 * a * b * b + constA;
+                bprim = 3 * a * a * b - b * b * b + constB;
+            }
+            else if (functionType == "z^4")
+            {
+                aprim = a * a * a * a - 6 * a * a * b * b + b * b * b * b + constA;
+                bprim = 4 * a * a * a * b - 4 * a * b * b * b + constB;
+            }
+            else if (functionType == "z^5")
+            {
+                aprim = a * a * a * a * a - 10 * a * a * a * b * b + 5 * a * b * b * b * b + constA;
+                bprim = 5 * a * a * a * a * b - 10 * a * a * b * b * b + b * b * b * b * b + constB;
+            }
+
+            a = aprim;
+            b = bprim;
+            modulus = Math.Sqrt(a * a + b * b);
+        } while (iteration < maxIter && modulus < 2);
+
+        return --iteration;
+    }
+
+    public static void Main()
+    {
+        Application.Run(new DrawPicture());
+    }
+    // End of class
+}
---
+++ Fraktaler/Function Mountain 2/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+public class DrawLandscape : Form
+{
+    double[,] grid = new double[257, 257];
+    Random rnd1 = new Random(1);
+        
+    // Välj hur höga topparna får vara
+    int MountainHeight = 100;
+
+    // Definiera färger 0..12
+    Color[] myColor = new Color[13] { Color.DarkBlue, Color.MediumBlue, Color.CornflowerBlue, Color.LightSkyBlue, Color.NavajoWhite,
+                                      Color.LightGreen, Color.LimeGreen, Color.DarkGreen, Color.Gray, Color.Silver, Color.LightGray, Color.Snow, Color.White};
+
+    public DrawLandscape()
+    {
+        for (int i = 0; i < 257; i++)
+        {
+            for (int j = 0; j < 257; j++)
+            {
+                grid[i, j] = 9999;
+            }
+        }
+
+        // Initiera skärmen med att anropa grafikrutinen
+        this.Text = "z = sin(r) + sin(2r)";
+        this.Size = new Size(1920, 1080);
+        this.Paint += new PaintEventHandler(SquareOne);
+    }
+
+    public void SquareOne(object sender, PaintEventArgs e)
+    {
+        // Utgå från den maximala rutstorleken 256*256 pixlar
+        int StartWidth = 256;
+        DivideSquare(e, 0, 0, StartWidth);
+    }
+
+    public double Function(double a, double b)
+    {
+        double xx = (a - 128) / 29.5;
+        double yy = (b - 128) / 29.5;
+        double r = Math.Sqrt(xx * xx + yy * yy);
+
+        return Math.Sin(r) + Math.Sin(2 * r) ;
+    }
+
+    public void DivideSquare(PaintEventArgs e, int px, int py, int CurWidth)
+    {
+        // Rutans nedre vänstra hörn = (px, py)
+        // Rutans bredd = w (värden 256, 128, 64, 32, 16...)
+
+        int NewWidth = CurWidth / 2;
+        double z1 = grid[px, py] = Function(px, py);
+        double z2 = grid[px, py + CurWidth] = Function(px, py + CurWidth);
+        double z3 = grid[px + CurWidth, py + CurWidth] = Function(px + CurWidth, py + CurWidth);
+        double z4 = grid[px + CurWidth, py] = Function(px + CurWidth, py);
+
+        // Ska rutan delas upp i mindre delar?
+        if (CurWidth > 4)
+        {
+            DivideSquare(e, px, py + NewWidth, NewWidth);
+            DivideSquare(e, px + NewWidth, py + NewWidth, NewWidth);
+            DivideSquare(e, px, py, NewWidth);
+            DivideSquare(e, px + NewWidth, py, NewWidth);
+        }
+        // Annars är denna ruta färdig att visas
+        else
+        {
+            // Räkna ut hörnpositioner för den aktuella rutan 
+            int x1 = 0 + (py + 0) * 2 + (px + 0) * 3;
+            int y1 = 480 + (px + 0) * 1 - (py + 0) * 2;
+            int x2 = 0 + (py + CurWidth) * 2 + (px + 0) * 3;
+            int y2 = 480 + (px + 0) * 1 - (py + CurWidth) * 2;
+            int x3 = 0 + (py + CurWidth) * 2 + (px + CurWidth) * 3;
+            int y3 = 480 + (px + CurWidth) * 1 - (py + CurWidth) * 2;
+            int x4 = 0 + (py + 0) * 2 + (px + CurWidth) * 3;
+            int y4 = 480 + (px + CurWidth) * 1 - (py + 0) * 2;
+
+            // Välj en färg 0..12
+            int LandColor = Convert.ToInt16(((z1 + z2 + z3 + z4)) * 50) + 512;
+            if (LandColor < 0) LandColor = 0;
+            if (z1 < -0.0) z1 = -0.0;
+            if (z2 < -0.0) z2 = -0.0;
+            if (z3 < -0.0) z3 = -0.0;
+            if (z4 < -0.0) z4 = -0.0;
+
+            // Måla den aktuella rutan på skärmen
+            Graphics g = e.Graphics;
+            SolidBrush MapBrush = new SolidBrush(Color.White);
+
+            int ColorTrans = 0;
+            int ColorRed   = 0;
+            int ColorGreen = 0;
+            int ColorBlue  = 0;
+            if (LandColor < 0) LandColor *= -1;
+
+            /* Black - Red */    if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = LandColor;       ColorGreen = 0;               ColorBlue = 0; }
+            else LandColor = LandColor - 256;
+            /* Red - Yelow */    if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = 255;             ColorGreen = LandColor;       ColorBlue = 0; }
+            else LandColor = LandColor - 256;
+            /* Yellow - Green */ if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = 256 - LandColor; ColorGreen = 255;             ColorBlue = 0; }
+            else LandColor = LandColor - 256;
+            /* Green - Teal */   if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = 0;               ColorGreen = 255;             ColorBlue = LandColor; }
+            else LandColor = LandColor - 256;
+            /* Teal - Blue */    if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = 0;               ColorGreen = 256 - LandColor; ColorBlue = 255; }
+            else LandColor = LandColor - 256;
+            /* Blue - Purple */  if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = LandColor;       ColorGreen = 0;               ColorBlue = 255; }
+            else LandColor = LandColor - 256;
+            /* Purple - White */ if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = 255;             ColorGreen = LandColor;       ColorBlue = 255; }
+            else LandColor = LandColor - 256;
+            /* White - Black */  if (ColorTrans == 0 && LandColor > 0 && LandColor < 256) { ColorTrans = 255; ColorRed = 256 - LandColor; ColorGreen = 256 - LandColor; ColorBlue = 256 - LandColor; }
+            else LandColor = LandColor - 256;
+
+            MapBrush.Color = 
+                Color.FromArgb(ColorTrans, // Specifies the transparency of the color.
+                               ColorRed,   // Specifies the amount of red.
+                               ColorGreen, // specifies the amount of green.
+                               ColorBlue); // Specifies the amount of blue.
+
+            e.Graphics.FillPolygon(MapBrush, new Point[]{
+                new Point(x1, y1 - Convert.ToInt16(z1 * MountainHeight)),
+                new Point(x2, y2 - Convert.ToInt16(z2 * MountainHeight)),
+                new Point(x3, y3 - Convert.ToInt16(z3 * MountainHeight)),
+                new Point(x4, y4 - Convert.ToInt16(z4 * MountainHeight))});
+
+            // Måla en ram runt rutan
+            Pen edgeBrush = new Pen(Color.DimGray, 1);
+            g.DrawPolygon(edgeBrush, new Point[]{
+                new Point(x1, y1 - Convert.ToInt16(z1 * MountainHeight)),
+                new Point(x2, y2 - Convert.ToInt16(z2 * MountainHeight)),
+                new Point(x3, y3 - Convert.ToInt16(z3 * MountainHeight)),
+                new Point(x4, y4 - Convert.ToInt16(z4 * MountainHeight))});
+        }
+    }
+
+    public static void Main()
+    {
+        Application.Run(new DrawLandscape());
+    }
+    // End of class
+}
